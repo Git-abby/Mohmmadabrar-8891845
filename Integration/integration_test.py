2212ac0dd45b9ffad92e0b2bb5a85616b49cd343
Git-abby <malekabrar56@gmail.com>
2024-02-13 18:17:43 -0500

    Changes have been made

--- Integration/integration_test.py
+++ Integration/integration_test.py
@@ -1,33 +1,10 @@
-# import addition
-# import multiplication
-import calculator
-def test_integration():
-    # Test multiplying the result of adding two numbers with another number
-    assert calculator.multiply(calculator.add(2, 3), 4) == 20
+import details
 
+def test_is_valid_email():
+    # assert details.is_valid_email("testmyemail.com")
 
+    assert details.is_valid_email("testmyemail@gmail.com")
 
-## Unit testing
-
-# def test_addition():
-#     assert calculator.add(2, 3) == 5
-#
-# def test_subtraction():
-#     assert calculator.subtract(5, 3) == 2
-#
-# def test_multiplication():
-#     assert calculator.multiply(2, 3) == 6
-#
-# def test_division():
-#     assert calculator.divide(6, 3) == 2
-#
-# def test_divide_by_zero():
-#     try:
-#         calculator.divide(6, 0)
-#     except ValueError as e:
-#         assert str(e) == "Cannot divide by zero"
-#     else:
-#         assert False, "Expected ValueError"
-#
-#
-#
+def test_is_valid_phone_number():
+    # assert details.is_valid_phone_number(+17898527869)
+    assert details.is_valid_phone_number(+78985278999888889969)
